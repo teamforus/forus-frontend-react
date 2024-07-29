cbe321d2692bc4b816cc8eff86863274d2302d58
Aleksei Loginov <lex.loginov@gmail.com>
2024-07-29 17:57:01 +0200

    add translations for aria label

--- react/src/webshop/layout/elements/LayoutMobileMenu.tsx
+++ react/src/webshop/layout/elements/LayoutMobileMenu.tsx
@@ -318,7 +318,7 @@ export default function LayoutMobileMenu() {
                                 className="button button-primary button-sm"
                                 role="button"
                                 onClick={() => startFundRequest({})}
-                                aria-label="login"
+                                aria-label="{translate('topnavbar.buttons.login')}"
                                 id="login_mobile">
                                 <em className="mdi mdi-account icon-start" />
                                 {translate('topnavbar.buttons.login')}
