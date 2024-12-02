26c3b36f5adfd5371ba2115a060f8a2fe475b343
dev-rminds <dev@rminds.nl>
2024-12-02 13:17:33 +0200

    update translations

--- react/src/dashboard/i18n/nl/modals/danger-zone/exclude_pre_check_fund.js
+++ react/src/dashboard/i18n/nl/modals/danger-zone/exclude_pre_check_fund.js
@@ -1,6 +1,7 @@
 export default {
-    title: 'Do you want to remove the fund from pre-check results?',
-    description: 'The fund will be removed from pre-check results',
+    title: 'Fonds verwijderen uit afwijkingen en uitsluitingen',
+    description:
+        'Door het fonds te verwijderen uit de afwijkingen wordt het weer meegenomen in de resultaten van de regelcheck. Op de website wordt het advies voor het fonds aan de gebruiker getoond, gebaseerd op de ingevulde antwoorden.',
     buttons: {
         cancel: 'Annuleren',
         confirm: 'Bevestigen',
