f4bea8012c55e54be610a5f03f3f0aefb74f541c
dev-rminds <dev@rminds.nl>
2025-02-03 18:12:16 +0200

    remove language_selector flag

--- react/src/webshop/hooks/useLangSelector.tsx
+++ react/src/webshop/hooks/useLangSelector.tsx
@@ -1,15 +1,13 @@
 import React, { useMemo } from 'react';
 import SelectControl from '../../dashboard/components/elements/select-control/SelectControl';
 import SelectControlOptionsLang from '../../dashboard/components/elements/select-control/templates/SelectControlOptionsLang';
-import useEnvData from './useEnvData';
 import useMainContext from './useMainContext';
 
 export default function useLangSelector() {
-    const envData = useEnvData();
     const { language, languages, changeLanguage } = useMainContext();
 
     return useMemo(() => {
-        if (!envData?.config?.language_selector || languages?.length <= 1) {
+        if (languages?.length <= 1) {
             return null;
         }
 
@@ -23,5 +21,5 @@ export default function useLangSelector() {
                 onChange={(language: string) => changeLanguage(language)}
             />
         );
-    }, [changeLanguage, language, languages, envData?.config?.language_selector]);
+    }, [changeLanguage, language, languages]);
 }
