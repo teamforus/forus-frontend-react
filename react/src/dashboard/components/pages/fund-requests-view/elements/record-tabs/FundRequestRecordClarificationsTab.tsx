f835b39fe3697021488a05da9a14b3ae097c32cf
Yaroslav Kosterin <ateamcms@gmail.com>
2025-06-11 11:26:36 +0300

    update fund request clarification reply form

--- react/src/dashboard/components/pages/fund-requests-view/elements/record-tabs/FundRequestRecordClarificationsTab.tsx
+++ react/src/dashboard/components/pages/fund-requests-view/elements/record-tabs/FundRequestRecordClarificationsTab.tsx
@@ -19,13 +19,17 @@ export default function FundRequestRecordClarificationsTab({
                             <div className="clarification-item-icon mdi mdi-message-text text-primary" />
                             <MultilineText text={clarification.question} />
                         </div>
-                        <div className="clarification-item-answer">
-                            <div className="clarification-item-icon mdi mdi-message-text text-primary-light" />
-                            <MultilineText
-                                className={clarification.answered_at ? '' : 'text-muted'}
-                                text={clarification.answered_at ? clarification.answer : 'Geen antwoord...'}
-                            />
-                        </div>
+
+                        {(!clarification.answered_at || (clarification.answered_at && clarification.answer)) && (
+                            <div className="clarification-item-answer">
+                                <div className="clarification-item-icon mdi mdi-message-text text-primary-light" />
+                                <MultilineText
+                                    className={clarification.answered_at ? '' : 'text-muted'}
+                                    text={clarification.answered_at ? clarification.answer : 'Geen antwoord...'}
+                                />
+                            </div>
+                        )}
+
                         {clarification?.files?.length > 0 && (
                             <div className="clarification-item-attachments">
                                 <FundRequestRecordAttachmentsTab
