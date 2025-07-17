d91370a832ab370fa98b7b7a9844d31a56917513
Yaroslav Kosterin <ateamcms@gmail.com>
2025-07-17 10:42:37 +0300

    add tooltips to dashboard tables

---
+++ react/src/dashboard/i18n/nl/pages/implementation-social-media.js
@@ -0,0 +1,14 @@
+export default {
+    labels: {
+        icon: 'Icoon',
+        type: 'Soort',
+        url: 'URL',
+        title: 'Titel',
+    },
+    tooltips: {
+        icon: 'Icoon dat op de webshop wordt weergegeven.',
+        type: 'Type social media (bijv. YouTube, Facebook, Twitter).',
+        url: 'Link naar het social media kanaal.',
+        title: 'Titel zichtbaar bij hover op de webshop.',
+    },
+};
--- react/src/dashboard/i18n/nl/pages/reservations.js
+++ react/src/dashboard/i18n/nl/pages/reservations.js
@@ -24,4 +24,14 @@ export default {
         transaction_id: 'Transactie ID',
         transaction_state: 'Transactie status',
     },
+    tooltips: {
+        number: 'Het unieke nummer van de reservering binnen het Forus platform. Dit nummer wordt automatisch gegenereerd.',
+        product: 'Het aanbod waar een reservering voor is gemaakt met de bijbehorende prijs van dit aanbod.',
+        price: 'Het bedrag vanuit het tegoed waar een reservering mee is gemaakt.',
+        customer:
+            'De gegevens van de deelnemer die een reservering heeft gemaakt. Dit zijn o.a. het e-mailadres, de voornaam en de achternaam.',
+        reserved_at: 'De datum waarop de reservering is gemaakt.',
+        status: 'De huidige status van de reservering.',
+        amount_extra: 'Het bedrag dat door de deelnemer is bijbetaald.',
+    },
 };
