d47b7b03f02b0540eb9f2c44570b164f9cc0f827
Kyra-05 <kyra@forus.io>
2025-08-21 16:53:00 +0200

    fixes #4 WCAG-audit-2024-2025

--- react/src/webshop/services/ProductService.ts
+++ react/src/webshop/services/ProductService.ts
@@ -152,7 +152,7 @@ export class ProductService<T = Product> {
     }
 
     public transformProductAlternativeText(product: Product): string {
-        const default_text = 'logo aanbod ' + product.name;
+        const default_text = '';
         const provider_text = '.De aanbieder omschrijft het aanbod als volgt: ' + product.alternative_text;
 
         return default_text + (product.alternative_text ? provider_text : '');
