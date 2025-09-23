7328c3e322ffbeac77d22158442834c57abe39bf
Kyra-05 <kyra@forus.io>
2025-09-23 13:05:34 +0200

    fix: input field css

--- react/src/webshop/components/pages/funds-pre-check/elements/AnswerInputField.tsx
+++ react/src/webshop/components/pages/funds-pre-check/elements/AnswerInputField.tsx
@@ -1,27 +1,20 @@
 import type { AnswerInputFieldProps } from '../../../../props/types/PrecheckChatbotTypes';
 import { useChatbotProvider } from '../../../../contexts/ChatbotContext';
 import { useState } from 'react';
-// import { CircleChevronRight } from 'lucide-react';
 import React from 'react';
 
 export default function AnswerInputField(answerInput: AnswerInputFieldProps) {
     const { sendMessage } = useChatbotProvider();
     const [input, setInput] = useState('');
-    // TODO: fix css
-    // const divClass =
-    // 'flex w-2/3 justify-items-start items-center text-sm sm:text-base rounded-md border-3 border-gray-400 bg-gray-100';
-    // const inputClass = 'w-full focus:outline-none text-center';
 
     const handleSend = async () => {
         await sendMessage(input);
     };
 
-    const sendButtonClass = 'hover:text-gray-600 text-gray-400';
-
     return (
-        <div className="answer-input-wrapper font">
+        <div className="answer-input-wrapper">
             <input
-                className="form form-control answer-input"
+                className="form form-input answer-input"
                 placeholder="Typ hier..."
                 type={answerInput.inputType}
                 min={answerInput.inputType === 'number' ? '0' : undefined}
@@ -32,9 +25,8 @@ export default function AnswerInputField(answerInput: AnswerInputFieldProps) {
                     }
                 }}
             />
-            <button onClick={handleSend} className={sendButtonClass} aria-label={'stuur bericht'}>
-                {/* <CircleChevronRight />*/}
-                {/* TODO: add icon */}
+            <button onClick={handleSend} className="button-icon button button-text" aria-label={'stuur bericht'}>
+                <em className={'mdi mdi-chevron-right-circle-outline'}></em>
             </button>
         </div>
     );
