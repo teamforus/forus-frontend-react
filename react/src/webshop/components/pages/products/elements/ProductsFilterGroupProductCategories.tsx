5f5d16ee9ee40dc3ece63abfeb26c9979a8b40f8
dev-rminds <dev@rminds.nl>
2025-10-09 08:37:07 +0300

    add root product category feature to implementations

--- react/src/webshop/components/pages/products/elements/ProductsFilterGroupProductCategories.tsx
+++ react/src/webshop/components/pages/products/elements/ProductsFilterGroupProductCategories.tsx
@@ -5,6 +5,8 @@ import ProductCategory from '../../../../../dashboard/props/models/ProductCatego
 import useProductCategoryService from '../../../../../dashboard/services/ProductCategoryService';
 import useSetProgress from '../../../../../dashboard/hooks/useSetProgress';
 import ProductsFilterGroup from './base-group/ProductsFilterGroup';
+import useAppConfigs from '../../../../hooks/useAppConfigs';
+import { clickOnKeyEnter } from '../../../../../dashboard/helpers/wcag';
 
 export default function ProductsFilterGroupProductCategories({
     value,
@@ -17,6 +19,7 @@ export default function ProductsFilterGroupProductCategories({
 }) {
     const translate = useTranslate();
     const setProgress = useSetProgress();
+    const appConfig = useAppConfigs();
     const [productCategories, setProductCategories] = useState<Array<ProductCategory>>(null);
 
     const productCategoryService = useProductCategoryService();
@@ -46,14 +49,70 @@ export default function ProductsFilterGroupProductCategories({
         setProgress(0);
 
         productCategoryService
-            .list({ parent_id: 'null', used: 1, per_page: 1000 })
+            .list({
+                parent_id: appConfig?.implementation?.root_product_category_id ?? 'null',
+                per_page: 1000,
+                used: 1,
+            })
             .then((res) => setProductCategories(res.data.data))
             .finally(() => setProgress(100));
-    }, [productCategoryService, setProgress]);
+    }, [appConfig?.implementation?.root_product_category_id, productCategoryService, setProgress]);
 
     useEffect(() => {
         fetchProductCategories();
     }, [fetchProductCategories]);
+
+    if (appConfig?.implementation?.root_product_category_id) {
+        return (
+            <ProductsFilterGroup
+                dusk={'productFilterGroupProductCategories'}
+                title={translate('products.filters.category')}
+                controls={'product_categories'}
+                openByDefault={openByDefault}
+                content={(isOpen) => (
+                    <div
+                        id="product_categories"
+                        className="showcase-aside-group-body"
+                        role="group"
+                        aria-label={translate('products.filters.category')}
+                        hidden={!isOpen}>
+                        <div className="showcase-aside-block-options">
+                            {productCategories?.map((category) => {
+                                const isActive = value?.includes(category.id);
+
+                                return (
+                                    <div
+                                        key={category.id}
+                                        role="button"
+                                        tabIndex={0}
+                                        aria-pressed={isActive}
+                                        aria-label={category.name}
+                                        onClick={() => {
+                                            setValue(
+                                                value?.includes(category.id)
+                                                    ? value?.filter((id) => id !== category.id)
+                                                    : [...value, category.id],
+                                            );
+                                        }}
+                                        onKeyDown={(e) => clickOnKeyEnter(e, true)}
+                                        className={classNames(
+                                            'showcase-aside-block-option',
+                                            isActive && 'showcase-aside-block-option-active',
+                                        )}
+                                        data-dusk={'productFilterFundItem' + category.id}>
+                                        <div className="showcase-aside-block-option-check">
+                                            <em className="mdi mdi-check" aria-hidden="true" />
+                                        </div>
+                                        <div className="showcase-aside-block-option-name">{category.name}</div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            />
+        );
+    }
 
     return (
         <ProductsFilterGroup
